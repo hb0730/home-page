db1b53ca9f87b69d92c6e52262a6b7dbe02657a1
hb0730 <1278032416@qq.com>
2024-12-09 16:23:35 +0800

    style: 高度问题

--- components/modules/Times.vue
+++ components/modules/Times.vue
@@ -2,7 +2,7 @@
 </script>
 
 <template>
-  <div class="max-md:flex-col lg:h-12rem animate-[fade_0.3s] flex flex-row items-center justify-between min-w-90">
+  <div class="max-md:flex-col lg:h-14rem animate-[fade_0.3s] flex flex-row items-center justify-between min-w-90">
     <div class="widget" h-full w-full p-10>
       <WidgetHitokoto />
     </div>
