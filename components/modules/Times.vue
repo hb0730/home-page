3f2acb20bb88d37fe709dc3f1877c571d3ee46de
hb0730 <1278032416@qq.com>
2025-08-07 14:20:45 +0800

    chore: update

--- components/modules/Times.vue
+++ components/modules/Times.vue
@@ -3,7 +3,7 @@
 
 <template>
   <div class="max-md:flex-col lg:h-14rem animate-[fade_0.3s] flex flex-row items-center justify-between min-w-90">
-    <div class="widget" h-full w-full p-10>
+    <div class="widget" h-full w-full p-10 min-w-20>
       <WidgetHitokoto />
     </div>
     <div class="widget max-md:mt-10 lg:ml-10" h-full w-full p-5>
--- utils/weather.ts
+++ utils/weather.ts
@@ -29,11 +29,12 @@ export interface Weather {
  * Get the weather for a given location
  */
 export default function getWeather(): Promise<Weather> {
-  return ownthink()
+  return vvhan();
 }
 
 /**
  * 教书先生API https://api.oioweb.cn/doc/weather/GetWeather
+ * @deprecated
  */
 async function ownthink(): Promise<Weather> {
   const response: any = await $fetch('https://api.oioweb.cn/api/weather/GetWeather', { ignoreResponseError: true })
@@ -55,5 +56,33 @@ async function ownthink(): Promise<Weather> {
     temperature: `${res.condition.min_degree} ~ ${res.condition.max_degree}`,
     wind: res.condition.day_wind_direction,
     windPower: res.condition.day_wind_power,
+  }
+}
+
+/**
+ *
+ * vvhan API https://vvhan.com/api/weather
+ * @returns
+ */
+async function vvhan(): Promise<Weather> {
+  const response: any = await $fetch('https://api.vvhan.com/api/weather', { ignoreResponseError: true })
+  if(!response?.success){
+    return {
+      province: '',
+      city: '',
+      weather: '',
+      temperature: '',
+      wind: '',
+      windPower: '',
+    }
+  }
+  const res = response.data
+  return {
+    province: res.province??'',
+    city: response.city??'',
+    weather: res.type,
+    temperature: `${res.high} ~ ${res.low}`.replaceAll('°C', ''),
+    wind: res.fengxiang,
+    windPower: res.fengli.replace('级', ''),
   }
 }
